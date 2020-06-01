6d572c708408d9db916cc00871ca22f4b5d1f71b
N U R O ™ <me@nuro.sh>
2020-06-01 23:48:14 +0100

    :art: Formatted endpoints

--- api/basic.ts
+++ api/basic.ts
@@ -1,25 +1,25 @@
 import {
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult,
-    Context,
-} from 'https://deno.land/x/lambda/mod.ts';
-import { TurnipExchange } from '../mod.ts';
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "https://deno.land/x/lambda/mod.ts";
+import { TurnipExchange } from "../mod.ts";
 
 export async function handler(
-event: APIGatewayProxyEvent,
-context: Context
+  event: APIGatewayProxyEvent,
+  context: Context,
 ): Promise<APIGatewayProxyResult> {
-    const turnips: TurnipExchange = new TurnipExchange();
+  const turnips: TurnipExchange = new TurnipExchange();
 
-    const result = await turnips.get_all();
+  const result = await turnips.get_all();
 
-    console.log(`Response: ${JSON.stringify(result)}`);
+  console.log(`Response: ${JSON.stringify(result)}`);
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(result),
-        headers: {
-            'content-type': 'application/json',
-        },
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+    headers: {
+      "content-type": "application/json",
+    },
+  };
 }
