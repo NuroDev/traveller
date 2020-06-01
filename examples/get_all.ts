e12535720e62979a5cb7ca18d0f9f64be95a3a35
N U R O ™ <me@nuro.sh>
2020-06-01 22:42:35 +0100

    :art: Formatted module

--- examples/get_all.ts
+++ examples/get_all.ts
@@ -1,11 +1,11 @@
-import { TurnipExchange } from '../mod.ts';
+import { TurnipExchange } from "../mod.ts";
 
 (async () => {
-    const turnips = new TurnipExchange();
+  const turnips = new TurnipExchange();
 
-    const result = await turnips.get_all();
+  const result = await turnips.get_all();
 
-    console.log(result[0]);
-})().catch(err => {
-    throw new Error(err);
+  console.log(result[0]);
+})().catch((err) => {
+  throw new Error(err);
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,104 +1,102 @@
-const BASE_URL: string = 'https://api.turnip.exchange/islands';
+const BASE_URL: string = "https://api.turnip.exchange/islands";
 
 export interface params_schema {
-    top_islands: number;
-    min_price: number;
-    max_price: number;
+  top_islands: number;
+  min_price: number;
+  max_price: number;
 }
 
 export interface island_schema {
-    background: string;
-    category: string;
-    clown: number;
-    creationTime: string;
-    description: string;
-    discordOnly: number;
-    fee: number;
-    fruit: string;
-    heart: number;
-    hemisphere: string;
-    islandScore: number;
-    islandTime: string;
-    islander: string;
-    live: number;
-    maxQueue: number;
-    messageID: string;
-    name: string;
-    patreon: number;
-    patreonOnly: number;
-    poop: number;
-    queued: string;
-    rating: number;
-    ratingCount: number;
-    thumbsdown: number;
-    thumbsupt: number;
-    turnipCode: string;
-    turnipPrice: number;
-    watchlist: number;
+  background: string;
+  category: string;
+  clown: number;
+  creationTime: string;
+  description: string;
+  discordOnly: number;
+  fee: number;
+  fruit: string;
+  heart: number;
+  hemisphere: string;
+  islandScore: number;
+  islandTime: string;
+  islander: string;
+  live: number;
+  maxQueue: number;
+  messageID: string;
+  name: string;
+  patreon: number;
+  patreonOnly: number;
+  poop: number;
+  queued: string;
+  rating: number;
+  ratingCount: number;
+  thumbsdown: number;
+  thumbsupt: number;
+  turnipCode: string;
+  turnipPrice: number;
+  watchlist: number;
 }
 
 export class TurnipExchange {
-    private top_islands: number;
-    private min_price: number;
-    private max_price: number;
+  private top_islands: number;
+  private min_price: number;
+  private max_price: number;
 
-    constructor(params?: params_schema) {
-        this.top_islands = params?.top_islands || 1;
-        this.min_price = params?.min_price || 300;
-        this.max_price = params?.max_price || 1000;
-    }
+  constructor(params?: params_schema) {
+    this.top_islands = params?.top_islands || 1;
+    this.min_price = params?.min_price || 300;
+    this.max_price = params?.max_price || 1000;
+  }
 
-    public async get_all () {
-        // Fetch the data from the turnip exchange API
-        const response = await fetch(BASE_URL, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                islander: 'neither',
-                category: 'turnips',
-            }),
-        });
+  public async get_all() {
+    // Fetch the data from the turnip exchange API
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        islander: "neither",
+        category: "turnips",
+      }),
+    });
 
-        // Convert the response to json
-        const json = await response.json();
+    // Convert the response to json
+    const json = await response.json();
 
-        let islands: island_schema[] = <island_schema[]>json.islands;
+    let islands: island_schema[] = <island_schema[]> json.islands;
 
-        islands.forEach((island) => {
-            // TODO: Simplify
-            if (island.turnipPrice < this.min_price)
-            {
-                const index = islands.indexOf(island, 0);
+    islands.forEach((island) => {
+      // TODO: Simplify
+      if (island.turnipPrice < this.min_price) {
+        const index = islands.indexOf(island, 0);
 
-                if (index > -1) {
-                    islands.splice(index, 1);
-                }
-            }
+        if (index > -1) {
+          islands.splice(index, 1);
+        }
+      }
 
-            // TODO: Simplify
-            if (island.turnipPrice > this.max_price)
-            {
-                const index = islands.indexOf(island, 0);
+      // TODO: Simplify
+      if (island.turnipPrice > this.max_price) {
+        const index = islands.indexOf(island, 0);
 
-                if (index > -1) {
-                    islands.splice(index, 1);
-                }
-            }
-        });
+        if (index > -1) {
+          islands.splice(index, 1);
+        }
+      }
+    });
 
-        islands = islands.sort((a, b) => a.turnipPrice - b.turnipPrice);
+    islands = islands.sort((a, b) => a.turnipPrice - b.turnipPrice);
 
-        return islands;
-    }
+    return islands;
+  }
 
-    public async get(num?: number) {
-        const islands = await this.get_all();
+  public async get(num?: number) {
+    const islands = await this.get_all();
 
-        const num_islands: number = num || this.top_islands;
+    const num_islands: number = num || this.top_islands;
 
-        return islands.slice(Math.max(islands.length - num_islands, 0));
-    }
+    return islands.slice(Math.max(islands.length - num_islands, 0));
+  }
 }
