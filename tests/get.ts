9e8209e25d15c5de654dbba2514914903dc326b1
N U R O ™ <me@nuro.sh>
2020-06-01 22:19:51 +0100

    :tada: Initial commit

---
+++ tests/get.ts
@@ -0,0 +1,11 @@
+import { TurnipExchange } from '../mod.ts';
+
+(async () => {
+    const turnips = new TurnipExchange();
+
+    const result = await turnips.get(3);
+
+    console.log(result);
+})().catch(err => {
+    throw new Error(err);
+});
