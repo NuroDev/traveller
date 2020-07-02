f3c7f0239937c08a12caf146b8e595b5ef5cf3e9
N U R O ™ <bendixon50@gmail.com>
2020-07-02 10:26:22 +0100

    :sparkles: Added basic examples

---
+++ examples/get_all.ts
@@ -0,0 +1,11 @@
+import { TurnipExchange } from "../mod.ts";
+
+(async () => {
+  const turnips = new TurnipExchange();
+
+  const result = await turnips.get_all();
+
+  console.log(result[0]);
+})().catch((err) => {
+  throw new Error(err);
+});
